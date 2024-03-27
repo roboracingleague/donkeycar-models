cc5b160e4895617725a8a9ac47958efa6cc1981f
Muldec <34677746+Muldec@users.noreply.github.com>
2024-03-27 16:17:21 +0100

    Update install script to use symbolic link

--- hooks/install_hooks.sh
+++ hooks/install_hooks.sh
@@ -1,5 +1,15 @@
 # Usage: ./install_hooks.sh
 
-# Copy or symlink hooks to the repository
-cp -rf "./src/*" ".git/hooks"
-echo "Hooks installed”
+FILES="src/*"
+
+GIT_DIR=$(git rev-parse --git-dir)
+HOOKS_DIR=$( cd -- "${GIT_DIR}/hooks" && pwd )
+
+for f in $FILES
+do
+  echo "Installing $f file..."
+
+  ln -s $( realpath $f ) $HOOKS_DIR
+done
+
+echo "Hooks installed"
